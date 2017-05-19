2c4064c4b86ce68d23b825a1c8b173bbf8d78230
Kyle Brennan <kylos101@gmail.com>
2017-05-19 17:55:52 -0400

    Adding dashboard link

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -20,5 +20,4 @@ export class DashboardPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad DashboardPage');
   }
-
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,5 +10,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  openDashboard(): void {
+    this.navCtrl.push("DashboardPage");
+  }
 }
